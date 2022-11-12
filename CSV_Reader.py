8da282a47dcf0e86642d49649fe590846f93ad87
Mohammad Ali <112762827+MA-HN@users.noreply.github.com>
2022-11-12 21:41:25 +0330

    show readed data in file

--- CSV_Reader.py
+++ CSV_Reader.py
@@ -3,5 +3,7 @@ import csv
 from statistics import mean
 
 
-with open('CSV_Reader.py','r') as csvfile:
-    
+with open('table.csv','r') as csvfile:
+    reader = csv.reader( csvfile )
+    for row in reader:
+        print(row)

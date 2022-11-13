1ed24d9d46f66e477abe8349bda6ff1f7d7e7aa3
Mohammad Ali <112762827+MA-HN@users.noreply.github.com>
2022-11-13 15:50:49 +0330

    how work with generator

--- CSV_Reader.py
+++ CSV_Reader.py
@@ -5,6 +5,10 @@ from statistics import mean
 
 with open('table.csv','r') as csvfile:
     reader = csv.reader( csvfile )
-    for row in reader:
-        #print(row)
-        open('mean_table.csv', 'w','')
+    with open('mean_table.csv', 'w') as out_meanfile:
+        for row in reader:
+            #print(row)
+            name = row[0]
+            mean_grade = mean((float(grade) for grade in row[1:])) #generator experation 
+
+        

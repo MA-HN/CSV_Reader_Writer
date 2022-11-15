ef560380a33d3b32247a38b0206d157040504fef
Mohammad Ali <112762827+MA-HN@users.noreply.github.com>
2022-11-15 13:06:47 +0330

    open outfile and write in it

--- CSV_Reader.py
+++ CSV_Reader.py
@@ -5,10 +5,12 @@ from statistics import mean
 
 with open('table.csv','r') as csvfile:
     reader = csv.reader( csvfile )
-    with open('mean_table.csv', 'w') as out_meanfile:
+    with open('mean_table.csv', 'w',newline='') as out_meanfile:
+        writer = csv.writer(out_meanfile)
         for row in reader:
             #print(row)
             name = row[0]
-            mean_grade = mean((float(grade) for grade in row[1:])) #generator experation 
+            mean_grade = mean((float(grade) for grade in row[1:])) #generator experation 7
+            writer.writerow([name,mean_grade])
 
         
